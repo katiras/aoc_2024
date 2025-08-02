e1b9bd319ff34d462a253c4d9233bfca034be73a
katiras <thanoskat@gmx.com>
2025-08-02 14:17:41 +0300

    day 5 b

--- day_5_b/src/main.rs
+++ day_5_b/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     let mut updates: Vec<Vec<usize>> = Vec::new();
     let mut rules: Vec<Rule> = Vec::new();
 
-    let mut sum = 0;
+    let mut already_sorted_count = 0;
 
     for line in reader.lines().map_while(Result::ok) {
         if line.len() > 5 {
@@ -37,40 +37,18 @@ fn main() {
         }
     }
 
-    let mut unsorted_update_indexes: HashSet<usize> = HashSet::new();
-
-    for rule in &rules {
-        for (i, update) in updates.iter_mut().enumerate() {
-            if let (Some(before_index), Some(after_index)) = (
-                update.iter().position(|&x| x == rule.before),
-                update.iter().position(|&x| x == rule.after),
-            ) {
-                if before_index > after_index {
-                    unsorted_update_indexes.insert(i);
-                }
-            }
-        }
-    }
-
-    let mut unsorted_updates = <Vec<Vec<usize>>>::new();
-
-    for (i, update) in updates.iter().enumerate() {
-        if !unsorted_update_indexes.contains(&i) {
-            sum += update[update.len() / 2];
-        } else {
-            unsorted_updates.push(update.to_vec());
-        }
-    }
-
-    println!("{:?}", sum);
-
     let mut sorted_sum = 0;
 
-    for mut update in unsorted_updates {
-        update.sort_by(|a, b| compare(&rules, *a, *b));
-        sorted_sum += update[update.len() / 2]
+    for mut update in updates {
+        if update.is_sorted_by(|a, b| compare(&rules, *a, *b) == Ordering::Less) {
+            already_sorted_count += update[update.len() / 2];
+        } else {
+            update.sort_by(|a, b| compare(&rules, *a, *b));
+            sorted_sum += update[update.len() / 2]
+        }
     }
 
+    println!("{:?}", already_sorted_count);
     println!("{:?}", sorted_sum);
 }
 
